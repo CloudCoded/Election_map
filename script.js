4aae74e41b5f215fba721b98d0f2e90148f1207b
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-04-29 04:46:38 -0400

    vote figures

--- script.js
+++ script.js
@@ -18,3 +18,7 @@ createPolitician = function(name, partyColor){
 
 let akindolapo = createPolitician('Akindolapo Abdulazeez', [245, 141, 136]);
 let betsy = createPolitician('Betsy Rocks', [132, 17, 11]);
+
+
+betsy.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+akindolapo.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
