2fb41c4f3c17e473da176e7b342bc81bcd6c191e
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-04-29 04:21:17 -0400

    create politician

---
+++ script.js
@@ -0,0 +1,7 @@
+createPolitician = function(name, partyColor){
+    let politician = {};
+    politician.name = name;
+    politician.electionResults = null;
+    politician.totalVotes = 0;
+    politician.partyColor = partyColor;
+}
